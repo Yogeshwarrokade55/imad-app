fe46dd9c98290d867e88136fcf727055b0f2295c
Yogeshwarrokade55 <36513581+Yogeshwarrokade55@users.noreply.github.com>
2018-02-22 00:50:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one', function (req, res) {
+  res.send('Artical one has been requested and will be served here');
+});
+
+app.get('/artical-two', function (req, res) {
+  res.send('Artical two has been requested and will be served here');
+});
+
+app.get('/artical-Three', function (req, res) {
+  res.send('Artical Three has been requested and will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
